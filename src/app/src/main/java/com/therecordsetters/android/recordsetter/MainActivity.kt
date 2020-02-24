0e3f64a436982c8e2cca44f26b2db2e1c76a6a46
Twoods91 <twoods91@yahoo.com>
2020-02-24 03:03:59 -0500

    added button for camera functionality

--- src/app/src/main/java/com/therecordsetters/android/recordsetter/MainActivity.kt
+++ src/app/src/main/java/com/therecordsetters/android/recordsetter/MainActivity.kt
@@ -8,5 +8,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //button press action
+        take_pic.setOnClickListener{
+            var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(i, 123)
+        }
+    }
+
+    //what happens after camera call is complete
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 123 && resultCode == RESULT_OK)
+        {
+            //makes picture from camera into bitmap format
+            var bmp = data?.extras?.get("data") as Bitmap
+            //below line only shows that the picture is in our program
+            //imageView.setImageBitmap(bmp)
+        }
+
     }
 }
